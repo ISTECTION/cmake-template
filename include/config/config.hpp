3e3a97f7dd88cadfcfe61467460584fcc42d4490
Владислав <perfectionist.one.love@gmail.com>
2022-05-09 00:13:54 +0700

    add target_link_libraries - argparse

--- include/config/config.hpp
+++ include/config/config.hpp
@@ -8,6 +8,6 @@
 #define V1_VERSION_MAJOR         "1"
 #define V1_VERSION_MINOR         "0"
 #define V1_VERSION_PATCH         "0"
-#define V1_VERSION               "v1.0.0-2-g77d8497"
+#define V1_VERSION               "v1.0.0-3-gfe42ba7"
 
 #endif /// _CONFIG_H_IN
--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,57 @@
+
+/// : static library
 #include "../lib/hello_lib/include/lib.hpp"
 
+/// : submodule git
+#include "argparse/argparse.hpp"
 
+/// : config
+#include "config/config.hpp"
+
+/// : exception
 #include "exception/exception.hpp"
+
+/// : headers
 #include "header.hpp"
 
+
+/// : standart template library (STL)
 #include <iostream>
 #include <string>
 
 int main (int argc, const char* argv[]) {
-    if (argc == 3) {
+    argparse::ArgumentParser _prs(V1_PROJECT_NAME, V1_VERSION);
+
+    _prs.add_argument("first") .scan<'i',int>();
+    _prs.add_argument("second").scan<'i',int>();
+
+    try {
+        _prs.parse_args(argc, argv);
+
+        auto _input1 = _prs.get<int>("first");
+        auto _input2 = _prs.get<int>("second");
+
         std::cout
-            << argv[1] << " + "
-            << argv[2] << " = "
-            << add(std::stoi(argv[1]), std::stoi(argv[2])) << '\n';
+            << _input1 << " + "
+            << _input2 << " = "
+            << add(_input1, _input2) << '\n';
 
-            print_hello();
+        print_hello();  /// static library
 
-        return 0;
+    } catch(const std::runtime_error& err) {
+        constexpr std::size_t args_no_received { 2 };
+        std::cerr << err.what() << std::endl;
+        std::cerr <<    _prs    << std::endl;
+        std::exit(args_no_received);
+    }
 
-    } else
+    /// test exception
+    bool _is_err = false;
+    std::cin >> _is_err;
+
+    if (_is_err)
         throw exception(exception::TYPE::ARGC_ERROR);
+    /// test exception
+
+    return 0;
 }
