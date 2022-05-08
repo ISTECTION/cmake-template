77d8497011bbc54f2e90b7ff4807db0fb4966d55
Владислав <perfectionist.one.love@gmail.com>
2022-05-08 19:08:02 +0700

    rm file

--- include/config/config.hpp
+++ include/config/config.hpp
@@ -5,9 +5,9 @@
 #define V1_PROJECT_SOURCE_DIR    "C:/MEGA/NSTU/CODE/CMake/v.1"
 
 #define V1_PROJECT_NAME          "v1"
-#define V1_VERSION_MAJOR         "1"
-#define V1_VERSION_MINOR         "0"
-#define V1_VERSION_PATCH         "0"
-#define V1_VERSION               "1.0.0"
+#define V1_VERSION_MAJOR         ""
+#define V1_VERSION_MINOR         ""
+#define V1_VERSION_PATCH         ""
+#define V1_VERSION               ""
 
 #endif /// _CONFIG_H_IN
